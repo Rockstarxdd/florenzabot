6350af29dfcc145107d5343ac26fe5e3736d73a7
ROCKSTAR OP <88956892+T5FR0CK5T4R@users.noreply.github.com>
2021-12-10 08:31:44 +0530

    Update ping.py

--- innexiaBot/modules/ping.py
+++ innexiaBot/modules/ping.py
@@ -80,7 +80,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "𝐒𝐓𝐀𝐑𝐆𝐈𝐑𝐋 𝐎𝐏\n"
+        "🇵 🇴 🇳 🇬 \n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
